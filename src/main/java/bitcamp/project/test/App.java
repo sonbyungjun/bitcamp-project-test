12767a8667ead16fd6765ac626cd257129c4d7e7
minheejeon <yycel1357@naver.com>
2019-04-17 16:07:29 +0900

    아하~~~~~~

--- src/main/java/bitcamp/project/test/App.java
+++ src/main/java/bitcamp/project/test/App.java
@@ -2,6 +2,9 @@ package bitcamp.project.test;
 
 import bitcamp.project.test.service.DeviceService;
 
+
+    
+
 public class App {
 
   public static void main(String[] args) {
@@ -9,6 +12,15 @@ public class App {
     DeviceService d = new DeviceService();
     d.device();
     System.out.println("***************************");
+  
+    int a = 10;
+    int b = 5;
+    
+    Mul m = new Mul();
+    System.out.println(m.mul(a, b));
+    
+  
   }
+
 
 }
---
+++ src/main/java/bitcamp/project/test/Mul.java
@@ -0,0 +1,14 @@
+package bitcamp.project.test;
+
+public class Mul {
+
+ public int mul(int a, int b) {
+   System.out.println("10 + 5는 뭘까요 ? ");
+   return a * b;
+   
+ }
+  
+
+  
+
+}
