3cc18c9b9ca3c9afaebb9b553372a1ebe3e666c1
sijin-kim <tlwls456@naver.com>
2019-04-17 16:15:58 +0900

    마지막

--- src/main/java/bitcamp/project/test/App.java
+++ src/main/java/bitcamp/project/test/App.java
@@ -1,20 +1,37 @@
 
 package bitcamp.project.test;
 
+import bitcamp.project.test.service.DeviceService;
+import bitcamp.project.test.service.Minus;
 import bitcamp.project.test.service.Plus;
+import bitcamp.project.test.service.Square;
 
 public class App {
   public String getGreeting() {
     return "Hello world."; 
   }
-
-
-
-  public static void main(String[] args) {
-
    
-    
-    
+  public static void main(String[] args) {
+    System.out.println("hahahahahahahahahaha");
+    Minus m = new Minus();
+    m.minus();
+    System.out.println("hohohohohohoho");
+    System.out.println("---------------------------");
+    Square square = new Square();
+    square.execute();
+    System.out.println("---------------------------");
+
+    System.out.println("***************************");
+    DeviceService d = new DeviceService();
+    d.device();
+    System.out.println("***************************");
+    int a = 10;
+    int b = 5;
+
+    Mul m2 = new Mul();
+    System.out.println(m2.mul(a, b));
+   
+  
     Plus plus1 = new Plus();           
 
     plus1.plus(10,20);
---
+++ src/main/java/bitcamp/project/test/service/Minus.java
@@ -0,0 +1,7 @@
+package bitcamp.project.test.service;
+
+public class Minus {
+  public void minus() {
+    System.out.println("빼기 입니다");
+  }
+}
