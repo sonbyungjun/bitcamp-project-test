73582b057bab8799d0052560e52bb15f11e0440d
sijin-kim <tlwls456@naver.com>
2019-04-17 16:12:15 +0900

    아아아

--- src/main/java/bitcamp/project/test/App.java
+++ src/main/java/bitcamp/project/test/App.java
@@ -10,23 +10,11 @@ public class App {
 
 
 
-
-
-
-
   public static void main(String[] args) {
 
-    System.out.println("***************************");
-    DeviceService d = new DeviceService();
-    d.device();
-    System.out.println("***************************");
+   
     
-    int a = 10;
-    int b = 5;
     
-    Mul m = new Mul();
-    System.out.println(m.mul(a, b));
-
     Plus plus1 = new Plus();           
 
     plus1.plus(10,20);
