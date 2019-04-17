517a0a87adad01c67427b9246de59b56ba4bde99
minheejeon <yycel1357@naver.com>
2019-04-17 16:08:07 +0900

    다시커밋함

--- src/main/java/bitcamp/project/test/App.java
+++ src/main/java/bitcamp/project/test/App.java
@@ -1,13 +1,23 @@
 package bitcamp.project.test;
 
 import bitcamp.project.test.service.DeviceService;
+import bitcamp.project.test.service.Square;
 
 
     
 
 public class App {
 
+  public String getGreeting() {
+    return "Hello world.";
+  }
+
   public static void main(String[] args) {
+    System.out.println("---------------------------");
+    Square square = new Square();
+    square.execute();
+    System.out.println("---------------------------");
+
     System.out.println("***************************");
     DeviceService d = new DeviceService();
     d.device();
