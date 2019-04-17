5dc88128321b261828afe7d7a41741aa057e475d
minheejeon <yycel1357@naver.com>
2019-04-17 16:12:14 +0900

    zzzzzzzzz

--- src/main/java/bitcamp/project/test/App.java
+++ src/main/java/bitcamp/project/test/App.java
@@ -7,11 +7,7 @@ import bitcamp.project.test.service.Square;
     
 
 public class App {
-
-  public String getGreeting() {
-    return "Hello world.";
-  }
-
+  
   public static void main(String[] args) {
     
     System.out.println("hahahahahahahahahaha");
