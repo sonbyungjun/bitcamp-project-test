be26577f7e63de64ee8ec6af0d8016c01415ee3b
sijin-kim <tlwls456@naver.com>
2019-04-17 16:06:06 +0900

    테스트
    하이

--- src/main/java/bitcamp/project/test/App.java
+++ src/main/java/bitcamp/project/test/App.java
@@ -4,35 +4,24 @@ package bitcamp.project.test;
 import bitcamp.project.test.service.Plus;
 
 public class App {
-    public String getGreeting() {
-        return "Hello world."; 
-    }
-
-   
-     
-    
-
-
-
-    public static void main(String[] args) {
-      System.out.println("***************************");
-      DeviceService d = new DeviceService();
-      d.device();
-      System.out.println("***************************");
-      
-      int a = 10;
-      int b = 5;
-      
-      Mul m = new Mul();
-      System.out.println(m.mul(a, b));
-
-Plus plus1 = new Plus();           
-
-plus1.plus(10,20);
-    
-  
+  public String getGreeting() {
+    return "Hello world."; 
   }
 
 
-}
+
+
+
+
+
+  public static void main(String[] args) {
+
+
+    Plus plus1 = new Plus();           
+
+    plus1.plus(10,20);
+
+
+  }
+
 }
